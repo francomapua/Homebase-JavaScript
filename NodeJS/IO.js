8ef7bb64191efbbf312bffbe727ac03766f9dc4a
Franco Luis Mapua <fmapua@gmail.com>
2019-05-25 18:41:17 +0800

    Adding Files from Homebase-NodeJS

---
+++ NodeJS/IO.js
@@ -0,0 +1,9 @@
+const fse = require('fs-extra');
+
+// TODO: Resources
+// https://tutorialedge.net/javascript/nodejs/reading-writing-files-with-nodejs/
+// https://github.com/jprichardson/node-fs-extra
+// https://nodejs.org/api/fs.html 
+function writeFile(){
+
+}
