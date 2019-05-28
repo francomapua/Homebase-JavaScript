fcdadd2a05be9cdd1a80f464f2eadb44b7e64a72
Franco Luis Mapua <franco@migo.tv>
2019-05-28 18:37:46 +0800

    Adding string formatting functions

--- StringManipulation.js
+++ StringManipulation.js
@@ -7,6 +7,31 @@ function getMatchesWithPrefixSuffix(baseText, prefix, suffix) {
     }
     return matchArr;
 }
-
 var matchArr = getMatchesWithPrefixSuffix("this ((is)) and example text with ((proper)) text", "((", "))");
 console.log('matchArr :', matchArr);
+
+
+function objectSnakeToCamel(underscoreObject) {
+    var camelObject = {};
+    for (const underscoreKey in underscoreObject) {
+        if (underscoreObject.hasOwnProperty(underscoreKey)) {
+            var camelKey = this.stringUnderscoredToCamel(underscoreKey);
+            camelObject[camelKey] = underscoreObject[underscoreKey];
+        }
+    }
+    return camelObject;
+}
+
+function stringSnakeToCamel(str) {
+    return str.replace(/_([a-z])/g, function (g) {
+        return g[1].toUpperCase();
+    });
+}
+
+function stringCamelToSnake(str) {
+    return str.replace(/\.?([A-Z]+)/g, function (x, y) {
+        return "_" + y.toLowerCase()
+    }).replace(/^_/, "");
+}
+
+console.log("transmute: " + stringCamelToSnake("configFileLocation").toUpperCase());
